9d7f962d15a492a7979ed629a817f8e33a2a164e
Dalex19 <dearalex875@gmail.com>
2022-09-23 21:13:26 -0600

    style

--- src/components/viewprincipal/Dashboard.jsx
+++ src/components/viewprincipal/Dashboard.jsx
@@ -1,17 +1,19 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import logo from "../../assets/google-360suite.svg";
-import {BsThreeDotsVertical, FaListUl, BsCalendar2CheckFill} from "react-icons/all";
+import {
+  BsThreeDotsVertical,
+  FaListUl,
+  BsCalendar2CheckFill,
+} from "react-icons/all";
 import { Application } from "./Application";
 import { Categories } from "./Categories";
 import { taskContext } from "../context/TaskProvider";
-
 
 const Dashboard = () => {
   return (
     <div className="flex font-poppins min-h-[650px] md:h-3/4 w-[95%] lg:w-3/4 h-[780px] rounded-md bg-white justify-evenly">
       <MenuLeft />
       <Application />
-      
     </div>
   );
 };
@@ -32,21 +34,19 @@ const MenuLeft = () => {
 };
 
 const SubMenu = () => {
-  const {tasks} = useContext(taskContext)
+  const { tasks } = useContext(taskContext);
   return (
     <div className="text-sm text-gray-500 border-gray-300 border-r-4 h-full flex flex-col gap-1">
-      <div className="flex flex-col h-[20%] max-h-20 justify-around items-start pl-4 mt-2">
+      <div className="flex flex-col h-[20%] max-h-20 justify-around items-start pl-4 lg:pl-10 mt-2">
         <h2 className="flex items-center gap-2">
           <FaListUl />
-          My Tasks <span>{tasks.length}</span>
+          My Tasks <span className=" ml-8">{tasks.length}</span>
         </h2>
         <h2 className="flex items-center gap-2">
           <BsCalendar2CheckFill /> Activities
         </h2>
       </div>
-      
-        <Categories />
-      
+      <Categories />
     </div>
   );
 };
