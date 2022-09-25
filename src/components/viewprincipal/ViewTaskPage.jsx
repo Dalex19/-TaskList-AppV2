f78345696ad229baaf07134820a68fa22a3d9b19
Dalex19 <dearalex875@gmail.com>
2022-09-24 20:16:50 -0600

    refactor

---
+++ src/components/viewprincipal/ViewTaskPage.jsx
@@ -0,0 +1,31 @@
+import React, {useContext} from "react";
+import { taskContext } from "../context/TaskProvider";
+import ViewTasks from './ViewTasks'
+
+function ViewTaskPage() {
+    const { tasks, setTasks } = useContext(taskContext);
+    const removeTask = (id) => {
+        const updateTask = tasks.filter((item, index) => index !== id);
+        setTasks(updateTask);
+      };
+    const editTask = (task, id) => {
+        const updateList = tasks.map((item, index) => {
+            if(index === id) {
+                item = task
+            }
+            return item
+        }) 
+        setTasks(updateList)
+    }  
+  return (
+    <div
+     className="w-full h-[78%] bg-gray-300 pt-4 mx-auto flex flex-wrap justify-center gap-4"
+    >
+        {tasks.map((item, index) => (
+            <ViewTasks key={index} task={item} removeTask={removeTask} editTask={editTask} index={index}/>
+        ))}
+    </div>
+  );
+}
+
+export default ViewTaskPage;
