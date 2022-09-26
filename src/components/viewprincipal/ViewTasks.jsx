f2bfde564d847ba7ea3e754ee86cfa54eac827b5
Dalex19 <dearalex875@gmail.com>
2022-09-26 10:41:17 -0600

    style

--- src/components/viewprincipal/ViewTasks.jsx
+++ src/components/viewprincipal/ViewTasks.jsx
@@ -8,7 +8,7 @@ const ViewTasks = ({ task, removeTask, index, editTask }) => {
   return (
     <>
       {!updateTask ? (
-        <div className="w-[200px] h-[150px] border-2 border-white flex flex-col justify-around rounded-lg transition-all ease-in-out hover:scale-105">
+        <div className="w-[200px] h-[150px] relative border-2 border-white flex flex-col justify-around rounded-lg transition-all ease-in-out hover:scale-105">
           <h3 className=" flex justify-around items-center font-semibold text-violet-600">
             {task.title}
             <div className="flex gap-2 text-lg text-black">
@@ -27,6 +27,9 @@ const ViewTasks = ({ task, removeTask, index, editTask }) => {
             </div>
           </h3>
           <p className="pl-2">{task.description}</p>
+          <span className="border text-xs bg-purple-500 font-bold w-14 absolute bottom-2 right-1 text-white text-center rounded-md">
+            {task.categorie}
+          </span>
         </div>
       ) : (
         <TaskEdit
