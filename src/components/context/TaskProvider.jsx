b7f27c684bd7acbb1ca748d663bb8dd2536ce7f3
Dalex19 <dearalex875@gmail.com>
2022-09-27 21:27:58 -0600

    feat: filter tasks by category

--- src/components/context/TaskProvider.jsx
+++ src/components/context/TaskProvider.jsx
@@ -1,24 +1,63 @@
-import React, {createContext, useState} from 'react'
+import React, { createContext, useState, useEffect } from "react";
 
-export const taskContext = createContext()
+export const taskContext = createContext();
 
-export const TaskProvider = ({children}) => {
-    const [tasks, setTasks] = useState([
-      {title: 'Cenar', description: 'Comer comidita saludable', categorie: 'Food'},
-      {title: 'Salud', description: 'Ejercitarme al menos 15 minutos al dia', categorie: 'Exercise'},
-      {title: 'Cenar', description: 'Comer comidita saludable', categorie: 'Food'},
-      {title: 'Salud', description: 'Ejercitarme al menos 15 minutos al dia', categorie: 'Exercise'}
-    ])
-    const addTask = (task) => {
-        setTasks([...tasks, task])
+export const TaskProvider = ({ children }) => {
+  const [tasks, setTasks] = useState([
+    {
+      title: "Cenar",
+      description: "Comer comidita saludable",
+      categorie: "Food",
+    },
+    {
+      title: "Salud",
+      description: "Ejercitarme al menos 15 minutos al dia",
+      categorie: "Exercise",
+    },
+    {
+      title: "Cenar",
+      description: "Comer comidita saludable",
+      categorie: "Food",
+    },
+    {
+      title: "Salud",
+      description: "Ejercitarme al menos 15 minutos al dia",
+      categorie: "Exercise",
+    },
+  ]);
+  const [taskView, setTaskView] = useState(tasks);
+  useEffect(() => {
+    setTaskView(tasks);
+  }, [tasks]);
+  const changeTask = (categorie) => {
+    if (categorie === "all") {
+      setTaskView(tasks);
+    } else {
+      const taskUpdate = tasks.filter(
+        (task) => task.categorie === categorie
+      );
+      setTaskView(taskUpdate);
     }
-    const removeTask = id => {
-      const taskFilters = tasks.filter((item, index) => index !== id)
-      setTasks(taskFilters)
-    }
+  };
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+  const removeTask = (id) => {
+    const taskFilters = tasks.filter((item, index) => index !== id);
+    setTasks(taskFilters);
+  };
   return (
-    <taskContext.Provider value={{tasks, addTask, setTasks ,removeTask}}>
-       {children} 
+    <taskContext.Provider
+      value={{
+        taskView,
+        tasks,
+        addTask,
+        setTasks,
+        removeTask,
+        changeTask,
+      }}
+    >
+      {children}
     </taskContext.Provider>
-  )
-}
+  );
+};
