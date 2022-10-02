866535af436035d3d9fb0ce348e9893ace99a397
Dalex19 <dearalex875@gmail.com>
2022-10-01 23:58:26 -0600

    add and use usercontext

--- src/components/viewprincipal/Application.jsx
+++ src/components/viewprincipal/Application.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { FiSearch, BsCalendar2DateFill } from "react-icons/all";
 import { Activities } from "./Activities";
 import ViewTaskPage from './ViewTaskPage'
 import { Task } from "./Task";
+import { userContext } from "../context/UserContext";
 
 export const Application = () => {
   const [viewTask, setViewTask] = useState(false);
@@ -19,11 +20,14 @@ export const Application = () => {
 
 const Header = () => {
   const [date, setDate] = useState("");
+  const { user } = useContext(userContext);
+
   useEffect(() => {
     const now = Date.now();
     const dateNow = new Date(now);
     setDate(dateNow.toLocaleDateString());
   }, []);
+
   return (
     <div className="flex items-center text-gray-500 border-gray-300  border-b-4 md:gap-1 justify-around w-full md:w-full h-[10%] text-sm mx-auto gap-6">
       <span className="bg-gray-300  md:w-[45%] rounded-lg flex items-center justify-center lg:flex-row gap-2 px-2 py-1 lg:w-3/12">
@@ -41,8 +45,8 @@ const Header = () => {
         />
       </div>
       <div className="flex flex-col lg:w-3/12  md:w-[45%] md:py-0 md:px-0  px-2 py-2">
-        <p className="">Hi Alex Cruz</p>
-        <p className="text-xs">dearalex875@gmail.com</p>
+        <p className="">Hi {(user.name) ? user.name : user.email}</p>
+        <p className="text-xs">{(!user.email) ? user.email : ''}</p>
       </div>
     </div>
   );
