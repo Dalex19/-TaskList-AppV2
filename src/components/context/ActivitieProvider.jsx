1128ba773f718ae3ea124d963ca3183e3fcce7d6
Dalex19 <dearalex875@gmail.com>
2022-10-02 22:46:56 -0600

    refactor code

--- src/components/context/ActivitieProvider.jsx
+++ src/components/context/ActivitieProvider.jsx
@@ -1,23 +1,25 @@
+import React, { createContext, useState } from "react";
 
-import React,{createContext, useState} from 'react'
+export const activiteContext = createContext();
 
-export const activiteContext = createContext()
+export const ActivitieProvider = ({ children }) => {
+  const [cate, setCate] = useState([]);
+  const createdCategorie = (newCate) => {
+    !cate.find((cate) => cate.name == newCate)
+      ? setCate([...cate, { name: newCate }])
+      : "";
+  };
+  const deleteCate = (indexItem) => {
+    setCate((prevState) =>
+      prevState.filter((cate, index) => index !== indexItem)
+    );
+  };
 
-export const ActivitieProvider = ({children}) => {
-    const [cate, setCate] = useState([]);
-      const createdCategorie = (newCate) => {
-        !cate.find((cate) => cate.name == newCate)
-            ? setCate([...cate, {name: newCate}]) : ''
-      }
-      const deleteCate = (indexItem) => {
-        setCate((prevState) => 
-            prevState.filter((cate, index) => index !== indexItem)
-        )
-      }
-
-    return(
-        <activiteContext.Provider value={{cate, createdCategorie, deleteCate}}>
-            {children}
-        </activiteContext.Provider>
-    )
-}
+  return (
+    <activiteContext.Provider
+      value={{ cate, createdCategorie, deleteCate }}
+    >
+      {children}
+    </activiteContext.Provider>
+  );
+};
